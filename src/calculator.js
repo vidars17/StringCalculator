e57c1db9ff666eba1d0659c862f88132cef244c9
vidars17 <vidarsigurds@gmail.com>
2018-10-08 17:21:24 +0000

    Add now returns the same number that is entered if there is only one.

--- src/calculator.js
+++ src/calculator.js
@@ -2,5 +2,9 @@ function Add(numbers) {
   if(numbers == "") {
     return 0;
   }
+
+
+
+  return parseInt(numbers);
 }
 module.exports = Add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,3 +3,7 @@ const add = require("./calculator");
 it("Should return 0 for an empty string.", () => {
   expect(add("")).toBe(0);
 });
+
+it("Should return same number if a single number.", () => {
+  expect(add("5")).toBe(5);
+});
